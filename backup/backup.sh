1c9cbcd1d302072d79627012371a44a022b80cac
Pavel Ogurtsov <cucumb3rpasha@gmail.com>
2024-07-22 11:04:55 +0300

    remove usells

--- backup/backup.sh
+++ backup/backup.sh
@@ -9,5 +9,3 @@ BACKUP_FILE="$DEST_DIR/backup_$(date +%Y%m%d_%H%M%S).tar.gz"
 tar -czvf $BACKUP_FILE $SOURCE_DIR
 
 find $DEST_DIR -name "*.tar.gz" -type f -mtime +7 -exec rm -f {} \;
-
-echo "Резервное копирование завершено: $BACKUP_FILE"
